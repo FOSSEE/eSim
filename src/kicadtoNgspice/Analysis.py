f1b4bdfc2689e4017d61f34e6c59bdc5ac307a15
athulappadan <a4athulappadan@gmail.com>
2017-12-04 18:07:01 +0530

    minor change

--- src/kicadtoNgspice/Analysis.py
+++ src/kicadtoNgspice/Analysis.py
@@ -78,6 +78,7 @@ class Analysis(QtGui.QWidget):
                 self.trbox.setDisabled(True)
                 self.check.setChecked(True)
                 self.track_obj.set_CheckBox["ITEMS"] ="DC"
+
         except:
             self.checkTRAN.setChecked(True)
             self.track_obj.set_CheckBox["ITEMS"] = "TRAN"
