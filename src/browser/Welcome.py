238fc9fa6b48cb9263f832c00f6bb19d4021682f
fossee <you@example.com>
2019-09-09 09:34:48 +0530

    removed license issue

--- src/browser/Welcome.py
+++ src/browser/Welcome.py
@@ -12,7 +12,7 @@ class Welcome(QtGui.QWidget):
         self.browser = QtGui.QTextBrowser()
         self.browser.setSource(QtCore.QUrl("../browser/pages/welcome.html"))
         self.browser.setOpenExternalLinks(True)
-	self.browser.setVerticalScrollBarPolicy(QtCore.Qt.ScrollBarAlwaysOff)
+        self.browser.setVerticalScrollBarPolicy(QtCore.Qt.ScrollBarAlwaysOff)
                 
         self.vlayout.addWidget(self.browser)  
         self.setLayout(self.vlayout)
