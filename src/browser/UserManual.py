30cecf651a7e9d5faf692bff709b4d87e6f9239f
rahulp13 <rahulp13@users.noreply.github.com>
2022-02-10 03:23:18 +0530

    Updated version details to v2.2 pre-release

--- src/browser/UserManual.py
+++ src/browser/UserManual.py
@@ -14,7 +14,7 @@ class UserManual(QtWidgets.QWidget):
 
         self.vlayout = QtWidgets.QVBoxLayout()
 
-        manual = 'library/browser/User-Manual/eSim_Manual_2.1.pdf'
+        manual = 'library/browser/User-Manual/eSim_Manual_2.2-pre_release.pdf'
 
         if os.name == 'nt':
             os.startfile(os.path.realpath(manual))
