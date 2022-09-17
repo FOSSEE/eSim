1b8addf7a0daa681014d2cef4371622b431b84e3
rahulp13 <rahulp13@users.noreply.github.com>
2022-09-17 19:12:48 +0530

    Moved gen-soc button to menu bar

--- src/frontEnd/Application.py
+++ src/frontEnd/Application.py
@@ -160,6 +160,21 @@ class Application(QtWidgets.QMainWindow):
         self.topToolbar.addAction(self.switchmode)
         self.topToolbar.addAction(self.helpfile)
 
+        self.soc = QtWidgets.QToolButton(self)
+        self.soc.setText('Generate SoC')
+        self.soc.setToolTip(
+            '<b>SPICE to Verilog Conversion</b><br>' + \
+            '<br>The feature is under development.' + \
+            '<br>It will be released soon.' + \
+            '<br><br>Thank you for your patience!!!'
+        )
+        self.soc.setStyleSheet(" \
+        QWidget { border-radius: 15px; border: 1px \
+            solid gray; padding: 10px; margin-left: 20px; } \
+        ")
+        self.soc.clicked.connect(self.showSoCRelease)
+        self.topToolbar.addWidget(self.soc)
+
         # This part is setting fossee logo to the right
         # corner in the application window.
         self.spacer = QtWidgets.QWidget()
@@ -246,6 +261,17 @@ class Application(QtWidgets.QMainWindow):
         self.lefttoolbar.addAction(self.omoptim)
         self.lefttoolbar.setOrientation(QtCore.Qt.Vertical)
         self.lefttoolbar.setIconSize(QSize(40, 40))
+
+
+    def showSoCRelease(self):
+        msg = '<b>SPICE to Verilog Conversion</b><br>' + \
+            '<br>The feature is under development.' + \
+            '<br>It will be released soon.' + \
+            '<br><br>Thank you for your patience!!!'
+        QtWidgets.QMessageBox.information(
+            self, 'SoC Generation', msg, QtWidgets.QMessageBox.Ok
+        )
+
 
     def closeEvent(self, event):
         '''
