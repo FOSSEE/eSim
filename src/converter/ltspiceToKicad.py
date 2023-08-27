fa32613efa4ae08f4dd432321d985e3ae880a460
Sangavi GR <21pw28@psgtech.ac.in>
2023-08-27 14:58:35 +0530

    Update ltspiceToKicad.py

--- src/converter/ltspiceToKicad.py
+++ src/converter/ltspiceToKicad.py
@@ -9,7 +9,7 @@ class LTspiceConverter:
     def __init__(self, parent):
         self.parent = parent
 
-    def convert(self, file_path):
+    def convert(self, file_path,convert_button):
         self.convert_button.clicked.disconnect()
         # Get the base name of the file without the extension
         filename = os.path.splitext(os.path.basename(file_path))[0]
@@ -18,7 +18,7 @@ class LTspiceConverter:
         # Check if the file is not empty
         if os.path.getsize(file_path) > 0:
             print("con lt")
-            self.convert_button.setEnabled(False)
+            convert_button.setEnabled(False)
         else:
             print("File is empty. Cannot perform conversion.")
             # A message box indicating that the file is empty
@@ -43,7 +43,7 @@ class LTspiceConverter:
                 return
             print(file_path)
             convert_button.setEnabled(True)
-            convert_button.clicked.connect(lambda: self.convert(file_path))
+            convert_button.clicked.connect(lambda: self.convert(file_path,convert_button))
             
         else:
             print("No file selected.")
