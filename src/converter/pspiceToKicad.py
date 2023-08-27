c72433d6c23ff7c420aa28b806abd0962dc69526
Sangavi GR <21pw28@psgtech.ac.in>
2023-08-27 15:48:39 +0530

    Update pspiceToKicad.py

--- src/converter/pspiceToKicad.py
+++ src/converter/pspiceToKicad.py
@@ -82,7 +82,7 @@ class PspiceConverter:
                 return
             print(file_path)
             convert_button.setEnabled(True)
-            convert_button.clicked.connect(self.convert(file_path,convert_button))
+            convert_button.clicked.connect(lambda: self.convert(file_path,convert_button))
             
         else:
             print("No file selected.")
