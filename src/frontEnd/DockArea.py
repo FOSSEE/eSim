2c096c0e44b5968d51dc575150c0b181de46dd49
Sangavi GR <21pw28@psgtech.ac.in>
2023-08-27 14:50:48 +0530

    Update DockArea.py

--- src/frontEnd/DockArea.py
+++ src/frontEnd/DockArea.py
@@ -197,12 +197,12 @@ class DockArea(QtWidgets.QMainWindow):
 
         upload_button1 = QPushButton("Upload Pspice schematics")
         upload_button1.setFixedSize(180, 30)
-        upload_button1.clicked.connect(lambda: self.pspice_converter.upload_file_Pspice(file_path_text_box.text()))
+        upload_button1.clicked.connect(lambda: self.pspice_converter.upload_file_Pspice(file_path_text_box.text(),self.convert_button))
         button_layout.addWidget(upload_button1)
 
         upload_button = QPushButton("Upload LTspice schematics")
         upload_button.setFixedSize(184, 30)
-        upload_button.clicked.connect(lambda: self.ltspice_converter.upload_file_LTspice(file_path_text_box.text()))
+        upload_button.clicked.connect(lambda: self.ltspice_converter.upload_file_LTspice(file_path_text_box.text(),self.convert_button))
         button_layout.addWidget(upload_button)
 
         self.convert_button = QPushButton("Convert Schematics to eSim")
