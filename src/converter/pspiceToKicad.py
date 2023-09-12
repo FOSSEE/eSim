14f56376f3fe054a4413c35a506b8816b6de842b
Sangavi GR <21pw28@psgtech.ac.in>
2023-09-12 13:40:45 +0530

    Update pspiceToKicad.py

--- src/converter/pspiceToKicad.py
+++ src/converter/pspiceToKicad.py
@@ -53,9 +53,15 @@ class PspiceConverter:
                     if workspace_directory:
                         print(f"{target_directory_name} is at: {workspace_directory}")
 
-                        #shutil.copytree(newFile, f"/home/ubuntus/eSim-Workspace/{filename}") 
-                        shutil.rmtree(f"{target_directory_name}/{filename}", ignore_errors=True)
-                        shutil.copytree(newFile, f"{target_directory_name}/{filename}")
+                        # #shutil.copytree(newFile, f"/home/ubuntus/eSim-Workspace/{filename}") 
+                        # shutil.rmtree(f"{target_directory_name}/{filename}", ignore_errors=True)
+                        # shutil.copytree(newFile, f"{target_directory_name}/{filename}")
+
+                        # Destination folder path (where you want to copy the file)
+                        destination_folder = "/path/to/destination/folder"
+
+                        # Use shutil.copy to copy the file to the destination folder
+                        shutil.copy(newFile, workspace_directory)
 
                         print("File added under the project explorer.")
                         # Message box with the Added Successfully message
