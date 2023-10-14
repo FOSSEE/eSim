946d7472dac722fd1c4d560c8f4215e96856fcc1
Sangavi GR <21pw28@psgtech.ac.in>
2023-10-14 16:53:29 +0530

    Update libConverter.py

--- src/converter/libConverter.py
+++ src/converter/libConverter.py
@@ -24,6 +24,7 @@ class PspiceLibConverter:
             parser_path = os.path.join(script_dir, relative_parser_path)
             print(parser_path)
             command = f"cd {parser_path} && python3 libParser.py {file_path}"
+            print(f"cd {parser_path} && python3 libParser.py {file_path}")
             try:
                 subprocess.run(command, shell=True, check=True)
                 # Message box with the conversion success message
