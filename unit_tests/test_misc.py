e32bad2666a955425094bd4fd465c7d36fcc71a3
vipul-09 <vipulkverma1@gmail.com>
2026-05-30 01:52:22 +0530

    Add unit tests for skipTo function

--- unit_tests/test_misc.py
+++ unit_tests/test_misc.py
@@ -17,3 +17,29 @@ def test_strip_num_from_ref_capacitor():
 
 def test_strip_num_from_ref_transistor():
     assert misc.stripNumFromRef("Q100") == "Q"
+
+from io import StringIO
+
+def test_skip_to_found():
+    stream = StringIO(
+        "abc\n"
+        "def\n"
+        "*symbol transistor\n"
+        "xyz\n"
+    )
+
+    result = misc.skipTo(stream, "*symbol")
+
+    assert result == "*symbol transistor"
+
+
+def test_skip_to_not_found():
+    stream = StringIO(
+        "abc\n"
+        "def\n"
+        "xyz\n"
+    )
+
+    result = misc.skipTo(stream, "*symbol")
+
+    assert result == "__ERROR__"
